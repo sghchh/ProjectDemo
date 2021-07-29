9004c321e77f348dad5be4166510d7c8f8174a61
71000011081075587324 <1325713032@qq.com>
2021-07-29 20:56:25 +0800

    加入了图片资源

--- app/src/main/java/com/starstudio/projectdemo/InfoFragment.java
+++ app/src/main/java/com/starstudio/projectdemo/InfoFragment.java
@@ -12,9 +12,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.starstudio.projectdemo.databinding.FragmentInfoBinding;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 public class InfoFragment extends Fragment {
 
@@ -26,7 +31,13 @@ public class InfoFragment extends Fragment {
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         binding = FragmentInfoBinding.inflate(inflater, container, false);
         configToolbar();
+        circleImage(null);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
@@ -35,11 +46,6 @@ public class InfoFragment extends Fragment {
         binding = null;
     }
 
-    // 用来构建appbar最右侧的按钮菜单
-    @Override
-    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-    }
 
     // 该方法用来配置toolbar
     private void configToolbar() {
@@ -47,5 +53,18 @@ public class InfoFragment extends Fragment {
         // 将Fragment的toolbar添加上
         ((AppCompatActivity)getActivity()).setSupportActionBar(binding.toolbarInfo.toolbar);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    //该方法用来将用户头像设置为圆形
+    private void circleImage(File file){
+        RequestOptions options = new RequestOptions()
+                .error(R.mipmap.nav_my_unselect)
+                .placeholder(R.mipmap.nav_my_unselect)
+                .transforms(new CircleCrop());
+
+        Glide.with(getContext())
+                .load(R.mipmap.nav_my_unselect)
+                .apply(options)
+                .into(binding.ivInfo);
     }
 }
