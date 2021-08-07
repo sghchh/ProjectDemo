da8f536155dad6716a048e1dae22eec1ab97d935
sgh <sgh2810@163.com>
2021-08-07 13:53:09 +0800

    天气与location api调试通过

--- app/src/main/java/com/starstudio/projectdemo/MainActivity.java
+++ app/src/main/java/com/starstudio/projectdemo/MainActivity.java
@@ -1,25 +1,37 @@
 package com.starstudio.projectdemo;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import androidx.annotation.RequiresApi;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hms.kit.awareness.Awareness;
+import com.huawei.hms.kit.awareness.CaptureClient;
+import com.huawei.hms.kit.awareness.capture.LocationResponse;
 import com.starstudio.projectdemo.Custom.HideInputActivity;
 import com.starstudio.projectdemo.databinding.ActivityMainBinding;
-import com.starstudio.projectdemo.journal.api.HmsImageService;
 import com.starstudio.projectdemo.utils.ContextHolder;
 import com.starstudio.projectdemo.utils.FileUtil;
 import com.starstudio.projectdemo.utils.RequestPermission;
 import com.starstudio.projectdemo.utils.SharedPreferencesUtils;
 
+import android.util.Log;
 import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
@@ -41,15 +53,19 @@ public class MainActivity extends HideInputActivity {
         // 进行权限申请的操作
         RequestPermission.init(this);
         permissionRequest = RequestPermission.getInstance();
-        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        String[] permissions = new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA, Manifest.permission.READ_PHONE_STATE};
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
         // 本次申请的权限是必要的，即没用通过则会导致APP无法使用
         permissionRequest.checkPermissions(RequestPermission.CODE_MUST, permissions);
 
         // 进行文件操作
         FileUtil.init(this);
-        HmsImageService.init(this);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -57,6 +73,7 @@ public class MainActivity extends HideInputActivity {
         sharedPreferencesUtils = SharedPreferencesUtils.getInstance(this);
 
         configView();
+
     }
 
     /*
@@ -78,10 +95,11 @@ public class MainActivity extends HideInputActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == RequestPermission.CODE_MUST)
-            for (int res : grantResults)
+            for (int res : grantResults) {
                 if (res != 0) {
-                    Toast.makeText(this, "相关权限未授权，无法使用~", Toast.LENGTH_SHORT).show();
-                    finish();
+                    Toast.makeText(this, "相关权限未通过,无法使用~", Toast.LENGTH_SHORT).show();
                 }
+            }
     }
+
 }
--- app/src/main/java/com/starstudio/projectdemo/journal/activity/JournalEditActivity.java
+++ app/src/main/java/com/starstudio/projectdemo/journal/activity/JournalEditActivity.java
@@ -1,11 +1,27 @@
 package com.starstudio.projectdemo.journal.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hms.kit.awareness.Awareness;
+import com.huawei.hms.kit.awareness.CaptureClient;
+import com.huawei.hms.kit.awareness.capture.CapabilityResponse;
+import com.huawei.hms.kit.awareness.capture.LocationResponse;
+import com.huawei.hms.kit.awareness.capture.WeatherStatusResponse;
+import com.huawei.hms.kit.awareness.status.WeatherStatus;
+import com.huawei.hms.kit.awareness.status.weather.Situation;
+import com.huawei.hms.kit.awareness.status.weather.WeatherSituation;
 import com.starstudio.projectdemo.databinding.Activity2JournalEditBinding;
+import com.starstudio.projectdemo.journal.api.HmsImageService;
 
 import java.util.ArrayList;
 
@@ -13,10 +29,45 @@ public class JournalEditActivity extends AppCompatActivity {
     Activity2JournalEditBinding binding;
     public ArrayList<String> picturePaths = new ArrayList<>();
     public int currentPostion = -1;
+    CaptureClient client;
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HmsImageService.init(this);
+        client = Awareness.getCaptureClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Awareness.getCaptureClient(this).getWeatherByDevice()
+                    // 执行成功的回调监听
+                    .addOnSuccessListener(new OnSuccessListener<WeatherStatusResponse>() {
+                        @Override
+                        public void onSuccess(WeatherStatusResponse weatherStatusResponse) {
+                            WeatherStatus weatherStatus = weatherStatusResponse.getWeatherStatus();
+                            WeatherSituation weatherSituation = weatherStatus.getWeatherSituation();
+                            Situation situation = weatherSituation.getSituation();
+                            // 更多返回的天气数据信息可参见华为开发者文档
+                            String weatherInfoStr = "City:" + weatherSituation.getCity().getName() + "\n" +
+                                    "Weather id is " + situation.getWeatherId() + "\n" +
+                                    "CN Weather id is " + situation.getCnWeatherId() + "\n" +
+                                    "Temperature is " + situation.getTemperatureC() + "℃" +
+                                    "," + situation.getTemperatureF() + "℉" + "\n" +
+                                    "Wind speed is " + situation.getWindSpeed() + "km/h" + "\n" +
+                                    "Wind direction is " + situation.getWindDir() + "\n" +
+                                    "Humidity is " + situation.getHumidity() + "%";
+                            Log.i("获取天气的结果", weatherInfoStr);
+                        }
+                    })
+                    // 执行失败的回调监听
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e("获取天气的结果", "get weather failed",e);
+                        }
+                    });
+        }
+
+
 
         binding = Activity2JournalEditBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
